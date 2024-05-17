a4ed2dfc15474773d73fdf6766b69a91ebb959c5
Pedro <pacnnunes@gmail.com>
2024-05-17 16:25:11 -0300

    update

--- front.py
+++ front.py
@@ -1,7 +1,6 @@
 import main as la
 import streamlit as st
 
-
 st.set_page_config(layout="wide")
 st.title("Decoder JWT")
 token = st.sidebar.text_area(label="Insira o JWT")
@@ -9,4 +8,4 @@ token = st.sidebar.text_area(label="Insira o JWT")
 if token:
     response = la.decode_jwt(token)
     st.text("Payload:")
-    st.text(response)
+    st.json(response)
